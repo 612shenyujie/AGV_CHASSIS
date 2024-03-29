3289f3e422fa044aa2978b5424c1b56ac50da928
Zhu junpeng <2261294472@qq.com>
2024-03-29 20:54:09 +0800

    添加buzzer库待使用

---
+++ GIMBAL/MDK-ARM/GIMBAL/buzzer_bsp.d
@@ -0,0 +1,4 @@
+gimbal\buzzer_bsp.o: ..\bsp\buzzer_bsp.c
+gimbal\buzzer_bsp.o: ..\bsp\buzzer_bsp.h
+gimbal\buzzer_bsp.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+gimbal\buzzer_bsp.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\math.h
